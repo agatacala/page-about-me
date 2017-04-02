7f6514d26a3bf15a8c1bdcf4d40d40b61a8aede4
agata.cala <agata.cala@gmail.com>
2017-04-02 20:15:12 +0200

    płynne przewijanie do elementów z menu

--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,8 +1,20 @@
 window.addEventListener('load', function() {
-    document.querySelector('.przejscie').addEventListener('click', function(e){
+
+    document.querySelector('.go-to-about-me').addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector('.about-me').scrollIntoView({behavior: "smooth"});
+    });
+
+    document.querySelector('.go-to-projects').addEventListener('click', function(e){
         e.preventDefault();
         document.querySelector('.projects').scrollIntoView({behavior: "smooth"});
-    })
+    });
+
+    document.querySelector('.go-to-contact').addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector('.contact').scrollIntoView({behavior: "smooth"});
+    });
+
     }
 );
 
